529247ed425e57e639e69e17b4e1d5b5d94bbf8c
Charlie <charlieryancurry@gmail.com>
2024-11-28 22:36:37 -0400

    add colors to terrain generation

--- Assets/Scripts/MeshGenerator.cs
+++ Assets/Scripts/MeshGenerator.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -10,9 +9,17 @@ public class MeshGenerator : MonoBehaviour
 
     Vector3[] verticies;
     int[] triangles;
+    Color[] colors;
 
     public int xSize = 20;
     public int zSize = 20;
+
+    public Gradient gradient;
+
+    private float minTerrainHeight;
+    private float maxTerrainHeight;
+
+    public float noiseMultiplier = 2;
 
     // Start is called before the first frame update
     void Start()
@@ -24,6 +31,19 @@ public class MeshGenerator : MonoBehaviour
         UpdateMesh();
     }
 
+    private void Update()
+    {
+        CreateShape();
+        UpdateMesh();
+        ResetMinMax();
+    }
+
+    public void ResetMinMax()
+    {
+        minTerrainHeight = 0;
+        maxTerrainHeight = 0;
+    }
+
     private void CreateShape()
     {
         verticies = new Vector3[(xSize + 1) * (zSize + 1)];
@@ -32,7 +52,18 @@ public class MeshGenerator : MonoBehaviour
         {
             for (int x = 0; x <= xSize; x++)
             {
-                verticies[i] = new Vector3(x, 0, z);
+                float y = Mathf.PerlinNoise((float)x * 0.3f, (float)z * 0.3f) * noiseMultiplier;
+                verticies[i] = new Vector3(x, y, z);
+
+                if (y > maxTerrainHeight)
+                {
+                    maxTerrainHeight = y;
+                }
+                if (y < minTerrainHeight)
+                {
+                    minTerrainHeight = y;
+                }
+
                 i++;
             }
         }
@@ -42,17 +73,33 @@ public class MeshGenerator : MonoBehaviour
         int tris = 0;
         int vert = 0;
 
-        for (int x = 0; x < xSize; x++)
+        for (int z = 0; z < zSize; z++)
         {
-            triangles[tris + 0] = vert + 0;
-            triangles[tris + 1] = vert + xSize + 1;
-            triangles[tris + 2] = vert + 1;
-            triangles[tris + 3] = vert + 1;
-            triangles[tris + 4] = vert + xSize + 1;
-            triangles[tris + 5] = vert + xSize + 2;
+            for (int x = 0; x < xSize; x++)
+            {
+                triangles[tris + 0] = vert + 0;
+                triangles[tris + 1] = vert + xSize + 1;
+                triangles[tris + 2] = vert + 1;
+                triangles[tris + 3] = vert + 1;
+                triangles[tris + 4] = vert + xSize + 1;
+                triangles[tris + 5] = vert + xSize + 2;
 
+                vert++;
+                tris += 6;
+            }
             vert++;
-            tris += 6;
+        }
+
+        colors = new Color[verticies.Length];
+
+        for (int i = 0, z = 0; z <= zSize; z++)
+        {
+            for (int x = 0; x <= xSize; x++)
+            {
+                float height = Mathf.InverseLerp(minTerrainHeight, maxTerrainHeight, verticies[i].y);
+                colors[i] = gradient.Evaluate(height);
+                i++;
+            }
         }
     }
 
@@ -63,15 +110,16 @@ public class MeshGenerator : MonoBehaviour
 
         mesh.vertices = verticies;
         mesh.triangles = triangles;
+        mesh.colors = colors;
 
         mesh.RecalculateNormals();
     }
 
-    private void OnDrawGizmos()
-    {
-        for (int i = 0;  i < verticies.Length; i++)
-        {
-            Gizmos.DrawSphere(verticies[i], 0.1f);
-        }
-    }
+    //private void OnDrawGizmos()
+    //{
+    //    for (int i = 0;  i < verticies.Length; i++)
+    //    {
+    //        Gizmos.DrawSphere(verticies[i], 0.1f);
+    //    }
+    //}
 }
